bdc759843a055ecf27d5db996629fccde2f84974
EvLiOsuna <31911287+EvLiOsuna@users.noreply.github.com>
2018-05-14 08:26:46 -0600

    Initial commit

---
+++ app/src/main/java/com/lily/contadorpasoslily/StepListener.java
@@ -0,0 +1,12 @@
+package com.lily.contadorpasoslily;
+
+/**
+ * Created by osaos on 10/05/2018.
+ */
+
+// Will listen to step alerts
+public interface StepListener {
+
+    public void step(long timeNs);
+
+}
